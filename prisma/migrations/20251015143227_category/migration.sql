2d67c0a2bc6fd19ffa3230a0a7d40df119615eaa
JustWhxt <tibo.hrn@gmail.com>
2025-10-16 14:31:26 +0200

    Add modif shema prisma

--- prisma/migrations/20251015143227_category/migration.sql
+++ prisma/migrations/20251015143227_category/migration.sql
@@ -2,10 +2,10 @@
   Warnings:
 
   - Added the required column `categoryId` to the `posts` table without a default value. This is not possible if the table is not empty.
-
 */
--- DropIndex
-DROP INDEX `posts_authorId_fkey` ON `posts`;
+
+-- DropForeignKey au lieu de DropIndex
+ALTER TABLE `posts` DROP FOREIGN KEY `posts_authorId_fkey`;
 
 -- AlterTable
 ALTER TABLE `posts` ADD COLUMN `categoryId` INTEGER NOT NULL;
@@ -13,12 +13,11 @@ ALTER TABLE `posts` ADD COLUMN `categoryId` INTEGER NOT NULL;
 -- CreateTable
 CREATE TABLE `categories` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- AddForeignKey
+-- AddForeignKey (recréation correcte)
 ALTER TABLE `posts` ADD CONSTRAINT `posts_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
--- AddForeignKey
+-- AddForeignKey (nouvelle clé vers category)
 ALTER TABLE `posts` ADD CONSTRAINT `posts_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
